36d67f7a6d0c104720b5ec7c4adb42d996f5921e
Sam Stevens <samuel.robert.stevens@gmail.com>
2022-03-30 13:43:03 -0400

    Initial commit.

---
+++ search/__main__.py
@@ -0,0 +1,166 @@
+import argparse
+import ast
+import dataclasses
+import os
+import pathlib
+import re
+from typing import Union, List, Literal, Iterator, Optional
+
+
+Part = Literal["all", "str"]
+
+
+def simple_log(*args, **kwargs):
+    print(*args, **kwargs)
+
+
+@dataclasses.dataclass
+class File:
+    content: str
+    path: pathlib.Path
+
+    def parse(self):
+        try:
+            return ast.parse(self.content, self.path)
+        except SyntaxError as err:
+            simple_log(f"Error parsing {self.path}:", err)
+            return None
+
+    def __getitem__(self, i):
+        lines = [""] + self.content.split("\n")
+        return lines[i]
+
+
+def _from_file(path: pathlib.Path) -> Optional[File]:
+    assert not path.is_dir()
+    if path.suffix != ".py":
+        return None
+
+    with open(path) as fd:
+        return File(fd.read(), path)
+
+
+def _from_dir(path: pathlib.Path) -> Iterator[File]:
+    assert path.is_dir()
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            filepath = pathlib.Path(dirpath) / filename
+            file = _from_file(filepath)
+            if file is not None:
+                yield file
+
+
+@dataclasses.dataclass
+class Options:
+    only_filenames: bool
+    invert_match: bool
+
+
+def new_options(args: argparse.Namespace) -> Options:
+    return Options(args.only_filenames, args.invert_match)
+
+
+def load_files(raw_paths: List[str]) -> Iterator[File]:
+    paths = map(pathlib.Path, raw_paths)
+
+    for path in paths:
+        if path.is_dir():
+            yield from _from_dir(path)
+        else:
+            file = _from_file(path)
+            if file is not None:
+                yield file
+
+
+class Searcher(ast.NodeVisitor):
+    def __init__(self, file: File, regexp: re.Pattern, options: Options):
+        self.file = file
+        self.regexp = regexp
+        self.options = options
+
+    def search(self, value) -> Union[bool, re.Match]:
+        match = self.regexp.search(value)
+        if match is None:
+            if self.options.invert_match:
+                return True
+            else:
+                return False
+        else:
+            if self.options.invert_match:
+                return False
+            else:
+                return match
+
+    def report(self, node: ast.AST, printfn):
+        printfn(self.file.path)
+
+        if self.options.only_filenames:
+            return
+
+        for lineno in range(node.lineno, node.end_lineno + 1):
+            printfn(f"{lineno}:{self.file[lineno]}")
+
+
+class String(Searcher):
+    def visit_Constant(self, node):
+        super().visit_Constant(node)
+
+        if not isinstance(node.value, str):
+            return
+
+        match = self.search(node.value)
+        if not match:
+            return
+
+        self.report(node, print)
+
+
+def search(file: File, part: Part, pattern: str, options: Options) -> None:
+    tree = file.parse()
+    if tree is None:
+        return
+
+    regexp = re.compile(pattern)
+
+    if part == "str":
+        visitor = String(file, regexp, options)
+    else:
+        assert part is None
+
+    visitor.visit(tree)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("regexp", help="Regular expression pattern to search for.")
+    parser.add_argument(
+        "part",
+        choices=["str", "all"],
+        help="What part of the codebase to look in. 'str' looks in string literals. 'all' looks at everything.",
+    )
+    parser.add_argument(
+        "paths", nargs="+", help="File or directory containing paths to search"
+    )
+    parser.add_argument(
+        "-l",
+        "--only-filenames",
+        action="store_true",
+        help="Only prints the filenames with a match.",
+    )
+    parser.add_argument(
+        "-v",
+        "--invert-match",
+        action="store_true",
+        help="Invert matching. Show nodes that do not match the given patterns.",
+    )
+    args = parser.parse_args()
+
+
+    options = new_options(args)
+
+    for file in load_files(args.paths):
+        search(file, args.part, args.regexp, options)
+
+
+if __name__ == "__main__":
+    main()
